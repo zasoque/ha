5020d4e6aced841acaf3e1aec1fa6f0ce770bc0d
Sch <junhg0211@gmail.com>
2026-03-02 13:03:45 +0900

    Update token cookie maxAge to expire in 1 week

--- src/routes/api/v1/auth/login/+server.ts
+++ src/routes/api/v1/auth/login/+server.ts
@@ -32,6 +32,7 @@ export const GET: RequestHandler = async ({ cookies, fetch, request }) => {
 
 	cookies.set('token', data.access_token, {
 		path: '/',
+		maxAge: 60 * 60 * 24 * 7, // 1 week
 		httpOnly: true,
 		secure: true,
 		sameSite: 'lax'
