38d1de423786c6aa0516082c09910cecb4ea6c2e
Sch <junhg0211@gmail.com>
2026-03-02 23:02:10 +0900

    Add 'diff' library for generating file differences

---
+++ src/routes/diff/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+	import { createTwoFilesPatch } from 'diff';
+
+	let one = '';
+	let two = '';
+
+	function diff() {
+		return createTwoFilesPatch('이전', '이후', one, two);
+	}
+</script>
+
+<div class="container">
+	<textarea bind:value={one}></textarea>
+	<textarea bind:value={two}></textarea>
+	<textarea readonly value={diff()}></textarea>
+</div>
+
+<style>
+	.container {
+		max-width: var(--max-width);
+		margin: 0 auto;
+		padding: 2rem;
+	}
+
+	textarea {
+		width: 100%;
+		display: block;
+		resize: none;
+		margin-bottom: 1rem;
+		height: 200px;
+	}
+</style>
