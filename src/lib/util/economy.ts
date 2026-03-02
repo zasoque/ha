296d0742ff14647919a0add524e660e22329d4e1
Sch <junhg0211@gmail.com>
2026-03-02 13:02:18 +0900

    Simplify formatCurrency function for currency display

--- src/lib/util/economy.ts
+++ src/lib/util/economy.ts
@@ -1,13 +1,5 @@
 export function formatCurrency(amount: string): string {
 	const formatted = amount.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 
-	if (formatted.startsWith('0.')) {
-		return `${parseInt(formatted.slice(2))}p̃`;
-	}
-
-	if (formatted.endsWith('.00')) {
-		return `${formatted.slice(0, -3)}Ñ`;
-	}
-
-	return `${formatted.slice(0, -3)}Ñ${formatted.slice(-2)}`;
+	return `Ñ${formatted}`;
 }
