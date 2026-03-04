d48c3dce2e8866eb88d95371e0a7fc59e431b2c2
Sch <junhg0211@gmail.com>
2026-03-04 19:26:43 +0900

    Remove newline at end of file and update textareas

--- src/routes/diff/+page.svelte
+++ src/routes/diff/+page.svelte
@@ -10,13 +10,13 @@
 			headerOptions: {
 				includeUnderline: false
 			}
-		}).replace(/\\ No newline at end of file/g, '\\ EOF');
+		}).replace(/\\ No newline at end of file(\n|$)?/g, '');
 	}
 </script>
 
 <Container>
-	<textarea bind:value={one}></textarea>
-	<textarea bind:value={two}></textarea>
+	<textarea bind:value={one} placeholder="현행"></textarea>
+	<textarea bind:value={two} placeholder="개정"></textarea>
 	<textarea readonly value={diff()}></textarea>
 </Container>
 
@@ -27,5 +27,6 @@
 		resize: none;
 		margin-bottom: 1rem;
 		height: 200px;
+		font-family: monospace;
 	}
 </style>
