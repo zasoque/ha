b3dd8c20f69c93f9adc724a9d5e0f05e1d18ba20
Sch <junhg0211@gmail.com>
2026-03-07 18:49:41 +0900

    Add Swagger documentation for GET owner endpoint

--- src/routes/api/v1/accounts/[accountId]/owner/+server.ts
+++ src/routes/api/v1/accounts/[accountId]/owner/+server.ts
@@ -1,6 +1,40 @@
 import { query } from '$lib/server/db';
 import { json, type RequestHandler } from '@sveltejs/kit';
 
+/**
+ * @swagger
+ * /api/v1/accounts/{accountId}/owner:
+ *   get:
+ *     summary: Get the owner of an account
+ *     parameters:
+ *       - in: path
+ *         name: accountId
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: The ID of the account
+ *     responses:
+ *       200:
+ *         description: The owner of the account
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                 owner:
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: integer
+ *                     name:
+ *                       type: string
+ *                     email:
+ *                       type: string
+ *       404:
+ *         description: Account not found
+ */
 export const GET: RequestHandler = async ({ params }) => {
 	const { accountId } = params;
 
