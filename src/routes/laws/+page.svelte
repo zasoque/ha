80e0e05d35d43483c4fe4fb3b095ec39fa5392bd
Sch <junhg0211@gmail.com>
2026-03-11 20:55:33 +0900

    Update law listings by category and level

--- src/routes/laws/+page.svelte
+++ src/routes/laws/+page.svelte
@@ -4,12 +4,63 @@
 
 	const { data } = $props();
 	const { laws } = $derived(data);
+
+	console.log(laws);
 </script>
 
 <Container>
 	<Title>법령정보</Title>
+	<div>헌법</div>
 	<ul>
-		{#each laws as law}
+		{#each laws.filter((l) => l.level === '헌법') as law}
+			<li>
+				<a href={`/laws/${law.id}`}>
+					{law.name}
+				</a>
+			</li>
+		{/each}
+	</ul>
+	<div>법률</div>
+	<ul>
+		{#each laws
+			.filter((l) => l.level === '법률')
+			.sort((a, b) => a.name.localeCompare(b.name)) as law}
+			<li>
+				<a href={`/laws/${law.id}`}>
+					{law.name}
+				</a>
+			</li>
+		{/each}
+	</ul>
+	<div>명령</div>
+	<ul>
+		{#each laws
+			.filter((l) => l.level === '명령')
+			.sort((a, b) => a.name.localeCompare(b.name)) as law}
+			<li>
+				<a href={`/laws/${law.id}`}>
+					{law.name}
+				</a>
+			</li>
+		{/each}
+	</ul>
+	<div>조례</div>
+	<ul>
+		{#each laws
+			.filter((l) => l.level === '조례')
+			.sort((a, b) => a.name.localeCompare(b.name)) as law}
+			<li>
+				<a href={`/laws/${law.id}`}>
+					{law.name}
+				</a>
+			</li>
+		{/each}
+	</ul>
+	<div>규칙</div>
+	<ul>
+		{#each laws
+			.filter((l) => l.level === '규칙')
+			.sort((a, b) => a.name.localeCompare(b.name)) as law}
 			<li>
 				<a href={`/laws/${law.id}`}>
 					{law.name}
